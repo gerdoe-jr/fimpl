5242092a164a62b11f2a5974e1e0062a7432e6d3
gerdoe-jr <gerdoe@gmail.com>
2024-07-20 23:12:13 +0300

    assertions?

--- tests/factory/src/main.cpp
+++ tests/factory/src/main.cpp
@@ -1,13 +1,21 @@
+#include <cassert>
 #include <iostream>
 
 #include <fimpl/factory.hpp>
 
 #include "interface.hpp"
 
+#define ASSERT(expr) \
+    { \
+        std::cout << "[" #expr "] is running..." << std::endl; \
+        assert(expr); \
+        std::cout << "[" #expr "] have been passed successfully!" << std::endl; \
+    };
+
 int main() {
     icontext *float_instance = create_instance<"float", icontext>();
     icontext *int_instance = create_instance<"int", icontext>();
 
-    std::cout << "float instance says: I'm " << float_instance->return_something() << std::endl;
-    std::cout << "int instance says: And I'm " << int_instance->return_something() << std::endl;
+    ASSERT(float_instance->return_something() == 24); 
+    ASSERT(int_instance->return_something() == 42);
 }
